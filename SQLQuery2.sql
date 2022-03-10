a1b3d7c92333eee3fb74f46e3a0f4b271eba7390
Praduman Kushwaha <Praduman Kushwaha@MR-1611>
2022-03-10 10:19:33 +0530

    Add project files.

---
+++ SQLQuery2.sql
@@ -0,0 +1,25 @@
+﻿select * from Locations
+
+
+insert into Locations values
+('Noida','this is best place'),
+('Delhi','this is best place'),
+('Bijnor','this is best place'),
+('Moradabad','this is best place'),
+('Muradnagar','this is best place'),
+('Meerut','this is best place')
+
+
+
+select * from customers
+insert into customers values
+('Ajay kumar','ajay7466@gmail.com','83847838398',1),
+('Sagar kumar','sagar09466@gmail.com','809843838398',2),
+('Arun kumar','arun00966@gmail.com','83898338398',4),
+('Ankush kumar','ankush9466@gmail.com','98647838398',3),
+('sachin kumar','sachin8466@gmail.com','8578787838398',3),
+('amit kumar','amit4466@gmail.com','8673838398',5)
+
+
+
+
---
+++ sagarCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace sagarCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
